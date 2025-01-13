d141334cd71b4f54a904dd36d05555882c330871
vikashgtcubetech <vikashsaini6640@gmail.com>
2025-01-13 14:18:09 +0530

    update banner

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -51,12 +51,12 @@ const Cart = () => {
             <hr />
             <div className="cart-total-details">
               <p>Delivery Fee</p>
-              <p>₹{getTotalCartAmount()===0?0:2}</p>
+              <p>₹{getTotalCartAmount()===0?0:20}</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <p>Total</p>
-              <p>₹{getTotalCartAmount()===0?0:getTotalCartAmount()+2}</p>
+              <p>₹{getTotalCartAmount()===0?0:getTotalCartAmount()+20}</p>
             </div>
           </div>
           <button onClick={()=>navigate('/order')}>PROCEED TO CHECKOUT</button>
